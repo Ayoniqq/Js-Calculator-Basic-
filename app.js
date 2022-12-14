88708cb149e4b0c68b6c556f4f984c4534a70261
Ayo <42424432+Ayoniqq@users.noreply.github.com>
2022-12-14 10:24:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+// This function display values and concatenating each new input to the existing one
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
 // This function clears all the values inputed on the screen
 function clearScreen() {
     document.getElementById("result").value = "";
-}
-
-// This function display values
-function display(value) {
-    document.getElementById("result").value += value;
 }
 
 // This function evaluates the expression and returns result
